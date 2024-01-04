e18ff864915094681aff37cc746d6152370d3e79
ganeshyandra <147488245+ganeshyandra@users.noreply.github.com>
2024-01-04 22:02:27 +0530

    #include<stdio.h>  int main()  {      double ta,tb;      scanf("%lf%lf",&ta,&tb);      double rc=1.0/ta+1.0/tb;      double tf=1.0/rc;      printf("%.lf ",tf);  }

--- Water_Taps.c
+++ Water_Taps.c
@@ -1,25 +1,10 @@
-  #include <stdio.h>
-
-int main() {
-    // Define variables for the time taken by Tap A and Tap B
-    double timeTapA, timeTapB;
-
-    // Ask the user to input the time taken by Tap A and Tap B to fill the tank
-    
-    scanf("%lf", &timeTapA);
-
-    
-    scanf("%lf", &timeTapB);
-
-    // Calculate the combined rate when both taps are working together
-    double rateCombined = 1.0 / timeTapA + 1.0 / timeTapB;
-
-    // Calculate the time to fill the tank
-    double timeToFill = 1.0 / rateCombined;
-
-    // Display the result
-    printf("%.f
-", timeToFill);
-
-    return 0;
-}
+ #include<stdio.h>
+ int main()
+ {
+     double ta,tb;
+     scanf("%lf%lf",&ta,&tb);
+     double rc=1.0/ta+1.0/tb;
+     double tf=1.0/rc;
+     printf("%.lf
+",tf);
+ }
